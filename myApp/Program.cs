7bede38a6b175610bbd9a776aee6b0b1b7081dfd
trevorzack <60204860+trevorzack@users.noreply.github.com>
2020-10-20 09:13:08 -0400

    Add files via upload
    Hello World

---
+++ myApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;   // Predefined namespace
+                // Way to distinguish classes
+
+namespace helloWorld    // user defined namespace
+{
+    class Program       
+    {
+        // execution starts here
+                    // Commandline arguments
+        static void Main(string[] args) // static means don't have to call main method explicitly
+        {
+            Console.WriteLine("Hello World! " + DateTime.Now);
+            string name;
+            Console.WriteLine("Please Enter your Name: ");
+            name = Console.ReadLine(); // takes user's input in string form
+
+            Console.WriteLine("Welcome " + name); // String concatenation: memory intensive
+            Console.WriteLine($"Welcome {name}"); // String interpolation 
+        }
+    }
+}
